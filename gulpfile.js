37a7c9b67a2a55a4bf0caa247d44ea2bef019307
Darren Cadwallader <darren@bitcrowd.net>
2016-03-30 14:17:05 +0200

    WIP - Added SC5-styleguide via npm - Added gulp tasks to generate - Added basic KSS to first few objects
    TODO:
    - Add KSS to all objects
    - Add KSS to all mixins
    - Ensure colours appear correctly
    - Tweak the appearance of the styleguide

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    scsslint = require('gulp-scss-lint');
+    scsslint = require('gulp-scss-lint'),
+    styleguide = require('sc5-styleguide'),
+    styleguideOutputPath = 'styleguide';
 
 gulp.task('build', function() {
   return gulp.src('stylesheets/bitstyles.scss')
@@ -12,6 +14,29 @@ gulp.task('lint', function() {
   return gulp.src('stylesheets/**/*.scss')
     .pipe(scsslint({'config': '.scss-lint.yml'}));
 });
+
+gulp.task('styleguide:generate', function() {
+  return gulp.src('stylesheets/**/*.scss')
+    .pipe(styleguide.generate({
+        title: 'Bitstyles',
+        server: true,
+        rootPath: styleguideOutputPath,
+        readOnly: true,
+        overviewPath: 'README.md'
+      }))
+    .pipe(gulp.dest(styleguideOutputPath));
+});
+
+gulp.task('styleguide:applystyles', function() {
+  return gulp.src('build/bitstyles.scss')
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(styleguide.applyStyles())
+    .pipe(gulp.dest(styleguideOutputPath));
+});
+
+gulp.task('styleguide', ['styleguide:generate', 'styleguide:applystyles']);
 
 gulp.task('watch', function() {
   gulp.watch('stylesheets/**/*.scss', ['styles', 'scss-lint']);
