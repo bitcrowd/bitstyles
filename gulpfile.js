2e22b9997077518f4c41d496eb5eda7e66d2cdce
Darren Cadwallader <darren@bitcrowd.net>
2016-04-04 17:53:58 +0200

    WIP - Adds icon svg - Started improving comments & example markup
    TODO:
    - hide the settins-flags from styleguide?
    - finish updating comments
    - ordering?

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,12 @@ gulp.task('styleguide:applystyles', function() {
     .pipe(gulp.dest(styleguideOutputPath));
 });
 
-gulp.task('styleguide', ['styleguide:generate', 'styleguide:applystyles']);
+gulp.task('styleguide:assets', function() {
+  return gulp.src('assets/images/*')
+    .pipe(gulp.dest('styleguide/assets/images/'));
+});
+
+gulp.task('styleguide', ['styleguide:generate', 'styleguide:applystyles', 'styleguide:assets']);
 
 gulp.task('watch', function() {
   gulp.watch('stylesheets/**/*.scss', ['styles', 'scss-lint']);
