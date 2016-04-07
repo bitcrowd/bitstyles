98f340debff4176fa06fc4f4d2cef578afc5450d
Darren Cadwallader <darren@bitcrowd.net>
2016-04-07 17:01:11 +0200

    - backstopjs visual regression testing now waits for event from styleguide generator (previously simply used a timeout) - Added the npm proxies for already-existing gulp commands - Updated README

---
+++ assets/javascripts/styleguide-init.js
@@ -0,0 +1,3 @@
+window.addEventListener('styleguide:onRendered', function() {
+  console.log('styleguideRendered'); // informs Backstop.js (visual regression test runner) that content is rendered.
+});
