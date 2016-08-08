6bcc43d85cee5b01b1068d646c5f452d3c8e1016
Darren Cadwallader <darren@bitcrowd.net>
2016-08-08 14:41:26 +0200

    Fixing paths

--- lib/bitstyles.rb
+++ lib/bitstyles.rb
@@ -1,3 +1,8 @@
 require "bitstyles/version"
 require "bitstyles/engine"
 
+bitstyles_path = File.expand_path("../../bitstyles/", __FILE__)
+ENV["SASS_PATH"] = [
+  ENV["SASS_PATH"],
+  bitstyles_path,
+].compact.join(File::PATH_SEPARATOR)
