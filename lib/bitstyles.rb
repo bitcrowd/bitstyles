8bc2f66625643a2b5a64074bc257ef5bebc32abd
Charlie Owen <charlie@sonniesedge.co.uk>
2016-09-21 11:55:18 +0200

    Use Engine for import

--- lib/bitstyles.rb
+++ lib/bitstyles.rb
@@ -1,8 +1,8 @@
 require "bitstyles/version"
 require "bitstyles/engine"
 
-bitstyles_path = File.expand_path("../../bitstyles/", __FILE__)
-ENV["SASS_PATH"] = [
-  ENV["SASS_PATH"],
-  bitstyles_path,
-].compact.join(File::PATH_SEPARATOR)
+# bitstyles_path = File.expand_path("../bitstyles/", __FILE__)
+# ENV["SASS_PATH"] = [
+#   ENV["SASS_PATH"],
+#   bitstyles_path,
+# ].compact.join(File::PATH_SEPARATOR)
--- lib/bitstyles/engine.rb
+++ lib/bitstyles/engine.rb
@@ -1,4 +1,9 @@
 module Bitstyles
   class Engine < Rails::Engine
+
+    initializer 'bitstyles.assets.precompile' do |app|
+      app.config.assets.paths << root.join('assets').to_s
+    end
+
   end
 end
