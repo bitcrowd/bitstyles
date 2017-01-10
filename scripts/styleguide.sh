e121fc6a5d9c52a5a48d439e847f4dbfff664119
Darren Cadwallader <darren@bitcrowd.net>
2017-01-10 10:52:03 +0100

    Initial pass over styleguide typography. Uses bitcrowd red branding, should be updated to new colours.

--- scripts/styleguide.sh
+++ scripts/styleguide.sh
@@ -10,7 +10,9 @@ styleguide \
   --server \
   --disableEncapsulation \
   --title 'bitstyles' \
+  --customColors "styleguide/assets/stylesheets/styleguide-colors.css" \
   --extraHead '<script src="/assets/javascripts/a11y-dialog.js"></script>' \
   --extraHead '<script src="/assets/javascripts/styleguide-init.js"></script>' \
   --extraHead '<script src="/assets/javascripts/modal.js"></script>' \
   --extraHead '<script src="/assets/javascripts/expander.js"></script>' \
+  --extraHead '<link rel="stylesheet" type="text/css" href="/assets/stylesheets/styleguide-theme.css">' \
