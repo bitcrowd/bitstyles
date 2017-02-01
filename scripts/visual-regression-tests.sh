eda5971449effdfd8ffe0ac1aa04626604c64f3d
Darren Cadwallader <darren@bitcrowd.net>
2017-02-01 13:59:45 +0100

    Second approach to getting VRTs on travis, creating a local master.

---
+++ scripts/visual-regression-tests.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -o errexit -o nounset
+
+temp_folder="./.test"
+git_url="https://github.com/bitcrowd/bitstyles.git"
+
+# Checkout master from GitHub
+mkdir $temp_folder
+git clone $git_url $temp_folder
+cd $temp_folder
+git checkout master
+phantomjs --version
+yarn
+
+# Generate reference images
+npm run styleguide &
+sleep 10
+npm run test:build
+kill $(ps aux | grep '[s]tyleguide' | awk '{print $2}')
+
+# copy then delete the new reference images
+cp -r ./test/backstop_data/bitmaps_reference/ ../test/backstop_data/bitmaps_reference/
+cd ../
+rm -rf $temp_folder
+
+# Generate test images
+npm run styleguide &
+sleep 10
+npm run test:compare
+kill $(ps aux | grep '[s]tyleguide' | awk '{print $2}')
