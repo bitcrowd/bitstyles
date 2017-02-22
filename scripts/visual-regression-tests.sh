70cebda45dbd2bbd29d85b5bbb4c527705c18afe
Darren Cadwallader <darren@bitcrowd.net>
2017-02-22 16:48:43 +0100

    removed VRT browser report

--- scripts/visual-regression-tests.sh
+++ scripts/visual-regression-tests.sh
@@ -1,16 +1,30 @@
 #!/bin/sh
 
+set -o nounset
+trap cleanup 1 2 15
+
 cleanup() {
   rm -rf "$temp_folder"
 }
 
-set -o nounset
-trap cleanup 1 2 15
+kill_children() {
+  local pid=$1
+
+  # kill its children
+  for child in $(pgrep -P "$pid"); do
+    kill_children "$child"
+  done
+
+  # kill it
+  if [ "$pid" != $$ ]; then
+    kill -0 "$pid" && kill -9 "$pid"
+  fi
+}
 
 temp_folder="./.test"
 git_url="https://github.com/bitcrowd/bitstyles.git"
 cache_file="../test/backstop_data/.cache"
-styleguide_startup_delay=5
+styleguide_startup_delay=8
 
 # Checkout master from GitHub
 mkdir $temp_folder
@@ -21,21 +35,20 @@ master_revision=$(git rev-parse master)
 
 # If there’s no local copy of reference images, or if they’re from an older
 # version of master, we generate new reference images
-if [ ! -f $cache_file ] || [ $master_revision != "$(<$cache_file)" ]; then
+if [ ! -f $cache_file ] || [ "$master_revision" != "$(cat $cache_file)" ]; then
   echo "Generating reference images"
   yarn
 
   npm run styleguide &
   styleguide_PID=$!
-  styleguide_PGID=$( ps -o pgid "$styleguide_PID" | grep "[0-9]" | tr -d ' ' )
   sleep $styleguide_startup_delay
   npm run test:build
-  kill -15 "$styleguide_PID"
-  kill -0 "-$styleguide_PGID" && kill -15 "-$styleguide_PGID"
 
   # Copy the new reference images up into the current branch
   cp -r ./test/backstop_data/bitmaps_reference ../test/backstop_data/
   echo "$master_revision" > $cache_file
+
+  kill_children $styleguide_PID
 fi
 
 cd ../ || exit 1
@@ -43,10 +56,8 @@ cd ../ || exit 1
 echo "Generating comparison images"
 npm run styleguide &
 styleguide_PID=$!
-styleguide_PGID=$( ps -o pgid "$styleguide_PID" | grep "[0-9]" | tr -d ' ' )
 sleep $styleguide_startup_delay
 npm run test:compare
-kill -15 "$styleguide_PID"
-kill -0 -"$styleguide_PGID" && kill -15 -"$styleguide_PGID"
 
+kill_children $styleguide_PID
 cleanup
