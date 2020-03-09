2e17d16ab4f51e137dbc05ec56e6daf11dd45ba6
Darren Cadwallader <darren@bitcrowd.net>
2020-03-09 17:43:34 +0100

    Couple more objects

---
+++ test/stories/objects/flex.stories.js
@@ -0,0 +1,10 @@
+export default { title: 'Objects/Flex' };
+
+export const flex = () => `
+  <ul class="o-flex o-list-reset">
+    <li>List item one</li>
+    <li class="o-flex__primary">Important content here</li>
+    <li>List item three</li>
+    <li>List item four</li>
+  </ul>
+`;
---
+++ test/stories/objects/hidden.stories.js
@@ -0,0 +1,11 @@
+export default { title: 'Objects/Hidden' };
+
+export const hidden = () => `
+  <p>Paragraph 1 content</p>
+  <p class="o-hidden">Paragraph 2 content</p>
+  <p>Paragraph 3 content</p>
+  <p class="o-hidden@small-only">Paragraph 4 content</p>
+  <p>Paragraph 5 content</p>
+  <p class="o-hidden@large">Paragraph 6 content</p>
+  <p>Paragraph 7 content</p>
+`;
