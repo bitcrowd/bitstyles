cfe86ef108bd3d118deef5e699e1a6b3ae060e91
Darren Cadwallader <darren@bitcrowd.net>
2020-03-18 12:29:09 +0100

    Moved cleafix to layout

---
+++ scss/bitstyles/layout/clearfix/clearfix.stories.js
@@ -0,0 +1,20 @@
+import Readme from './clearfix.md';
+
+export default {
+  title: 'Layout/Clearfix',
+  parameters: {
+    readme: {
+      sidebar: Readme,
+    },
+  },
+};
+
+export const clearfix = () => `
+  <div>
+    <div class="l-clearfix t-background-color--secondary">
+      <div class="l-clearfix" style="float: left;">Floated content here</div>
+      <div class="l-clearfix" style="float: left;">Floated content here</div>
+    </div>
+    <p>This content is cleared (try removing the clearfix class above, in your browser’s web inspector)</p>
+  </div>
+`;
