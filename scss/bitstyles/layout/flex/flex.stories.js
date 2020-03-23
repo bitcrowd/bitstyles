7f0e28848216693f7a1c378b33205d42c1124512
Darren Cadwallader <darren@bitcrowd.net>
2020-03-23 13:31:52 +0100

    - flex__primary now only sets flex-grow — the element will no longer cause the flex parent to overflow when it’s content is of unrestrained width (#321)
    - Adds the __no-shrink child class, and the --wrap modifier
    - Improves the examples, and the documentation

--- scss/bitstyles/layout/flex/flex.stories.js
+++ scss/bitstyles/layout/flex/flex.stories.js
@@ -17,3 +17,39 @@ export const flex = () => `
     <li>List item four</li>
   </ul>
 `;
+
+export const flexWithLongContent = () => `
+  <ul class="l-flex o-list-reset">
+    <li>Gingerbread topping tart. Bear claw cake icing cotton candy cheesecake biscuit pudding. Macaroon gummi bears chocolate bar lollipop. Caramels marzipan halvah fruitcake chocolate cake cake tart pudding. Bonbon sweet roll fruitcake soufflé topping fruitcake sesame snaps muffin. Pudding dragée marzipan ice cream topping chocolate cake fruitcake bear claw muffin.</li>
+    <li class="l-flex__primary u-background-color--secondary">Important content here - Gingerbread topping tart. Bear claw cake icing cotton candy cheesecake biscuit pudding. Macaroon gummi bears chocolate bar lollipop. Caramels marzipan halvah fruitcake chocolate cake cake tart pudding. Bonbon sweet roll fruitcake soufflé topping fruitcake sesame snaps muffin. Pudding dragée marzipan ice cream topping chocolate cake fruitcake bear claw muffin.</li>
+    <li>Gingerbread topping tart. Bear claw cake icing cotton candy cheesecake biscuit pudding. Macaroon gummi bears chocolate bar lollipop. Caramels marzipan halvah fruitcake chocolate cake cake tart pudding. Bonbon sweet roll fruitcake soufflé topping fruitcake sesame snaps muffin. Pudding dragée marzipan ice cream topping chocolate cake fruitcake bear claw muffin.</li>
+    <li>Gingerbread topping tart. Bear claw cake icing cotton candy cheesecake biscuit pudding. Macaroon gummi bears chocolate bar lollipop. Caramels marzipan halvah fruitcake chocolate cake cake tart pudding. Bonbon sweet roll fruitcake soufflé topping fruitcake sesame snaps muffin. Pudding dragée marzipan ice cream topping chocolate cake fruitcake bear claw muffin.</li>
+  </ul>
+`;
+
+export const flexWrapWithLongContent = () => `
+  <ul class="l-flex l-flex--wrap o-list-reset">
+    <li>Gingerbread topping tart. Bear claw cake icing cotton candy cheesecake biscuit pudding. Macaroon gummi bears chocolate bar lollipop. Caramels marzipan halvah fruitcake chocolate cake cake tart pudding. Bonbon sweet roll fruitcake soufflé topping fruitcake sesame snaps muffin. Pudding dragée marzipan ice cream topping chocolate cake fruitcake bear claw muffin.</li>
+    <li class="l-flex__primary u-background-color--secondary">Important content here - Gingerbread topping tart. Bear claw cake icing cotton candy cheesecake biscuit pudding. Macaroon gummi bears chocolate bar lollipop. Caramels marzipan halvah fruitcake chocolate cake cake tart pudding. Bonbon sweet roll fruitcake soufflé topping fruitcake sesame snaps muffin. Pudding dragée marzipan ice cream topping chocolate cake fruitcake bear claw muffin.</li>
+    <li>Gingerbread topping tart. Bear claw cake icing cotton candy cheesecake biscuit pudding. Macaroon gummi bears chocolate bar lollipop. Caramels marzipan halvah fruitcake chocolate cake cake tart pudding. Bonbon sweet roll fruitcake soufflé topping fruitcake sesame snaps muffin. Pudding dragée marzipan ice cream topping chocolate cake fruitcake bear claw muffin.</li>
+    <li>Gingerbread topping tart. Bear claw cake icing cotton candy cheesecake biscuit pudding. Macaroon gummi bears chocolate bar lollipop. Caramels marzipan halvah fruitcake chocolate cake cake tart pudding. Bonbon sweet roll fruitcake soufflé topping fruitcake sesame snaps muffin. Pudding dragée marzipan ice cream topping chocolate cake fruitcake bear claw muffin.</li>
+  </ul>
+`;
+
+export const flexWrapWithIntrinsicWidthContent = () => `
+  <ul class="l-flex l-flex--wrap o-list-reset">
+    <li><img src="https://placekitten.com/300/300" width="300" height="300" class="l-block" /></li>
+    <li class="l-flex__primary u-background-color--secondary">Important content here</li>
+    <li><img src="https://placekitten.com/300/300" width="300" height="300" class="l-block" /></li>
+    <li><img src="https://placekitten.com/300/300" width="300" height="300" class="l-block" /></li>
+  </ul>
+`;
+
+export const flexWithIntrinsicWidthContent = () => `
+  <ul class="l-flex o-list-reset">
+    <li><img src="https://placekitten.com/300/300" width="300" height="300" class="l-block" /></li>
+    <li class="l-flex__primary u-background-color--secondary">Important content here</li>
+    <li><img src="https://placekitten.com/300/300" width="300" height="300" class="l-block" /></li>
+    <li><img src="https://placekitten.com/300/300" width="300" height="300" class="l-block" /></li>
+  </ul>
+`;
