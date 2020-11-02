04942eafab5224bbfaeafe16eeaa9d1bd5371fb6
Darren Cadwallader <darren@bitcrowd.net>
2020-11-02 11:34:12 +0100

    Update button styles
    - Base button styles are now flexbox based
    - button--icon settings now match the base button, so more customisable, and more consistent
    - Updates button and button--icon stories, includes disabled styles

--- scss/bitstyles/objects/button/button.stories.js
+++ scss/bitstyles/objects/button/button.stories.js
@@ -15,6 +15,12 @@ export const buttonElement = () => `
   </button>
 `;
 
+export const buttonDisabled = () => `
+  <button class="o-button" disabled>
+    <span class="o-button__label">Button Text</span>
+  </button>
+`;
+
 export const buttonAnchorElement = () => `
   <a href="#" class="o-button">
     <span class="o-button__label">Button Text</span>
@@ -34,8 +40,8 @@ export const buttonWithIcon = () => `
   </button>
 `;
 
-export const buttonWithReversed = () => `
-  <button class="o-button {$modifiers}">
+export const buttonWithIconReversed = () => `
+  <button class="o-button">
     <span class="o-button__label">Button Text</span>
     <svg class="o-button__icon o-icon" viewBox="0 0 100 100" width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="m55.94 1.03v43h43v12h-43v43h-12v-43h-43v-12h43v-43z" fill-rule="evenodd"/></svg>
   </button>
