4bcb0c2bda7ee264f48650d92eb7589757138719
Darren Cadwallader <darren@bitcrowd.net>
2020-11-02 15:13:54 +0100

    Updates storybook & gets the build working again
    TODO:
    - setup a replacement for the storybook-readme plugin

--- .storybook/config.js
+++
@@ -1,6 +0,0 @@
-import '../build/bitstyles.css';
-import { addDecorator, addParameters } from '@storybook/html';
-import { addReadme } from 'storybook-readme/html';
-
-addParameters({ options: { theme: {} } });
-addDecorator(addReadme);
--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,3 @@
 module.exports = {
   stories: ['../scss/bitstyles/**/*.stories.js'],
-  addons: [
-    'storybook-readme/register',
-  ],
 };
