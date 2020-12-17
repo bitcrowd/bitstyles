c6859b643051e406183d31756a9a31340fae2f72
Darren Cadwallader <darren@bitcrowd.net>
2020-12-17 14:55:44 +0100

    [#354] Move hidden & visuallyhidden (#357)
    Both classes moved to utilities layer

--- scss/bitstyles/utilities/hidden/hidden.stories.js
+++ scss/bitstyles/utilities/hidden/hidden.stories.js
@@ -11,18 +11,18 @@ export default {
 
 export const hidden = () => `
   <p>Paragraph 1 content</p>
-  <p class="o-hidden">Paragraph 2 content</p>
+  <p class="u-hidden">Paragraph 2 content</p>
   <p>Paragraph 3 content</p>
 `;
 
 export const hiddenAtSmallOnly = () => `
   <p>Paragraph 1 content</p>
-  <p class="o-hidden@small-only">Paragraph 2 content</p>
+  <p class="u-hidden@small-only">Paragraph 2 content</p>
   <p>Paragraph 3 content</p>
 `;
 
 export const hiddenAtLarge = () => `
   <p>Paragraph 1 content</p>
-  <p class="o-hidden@large">Paragraph 2 content</p>
+  <p class="u-hidden@large">Paragraph 2 content</p>
   <p>Paragraph 3 content</p>
 `;
