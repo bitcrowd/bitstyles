389483c9f19f91b79f21c24401eb44b6f62d22f6
Darren Cadwallader <darren@bitcrowd.net>
2020-12-17 15:12:55 +0100

    Update button styles (#355)
    - Base button styles are now flexbox based
    - button--icon settings now match the base button, so more customisable, and more consistent
    - Updates button and button--icon stories, includes disabled styles
    
    * Adds box-shadows to buttons’ settings

--- scss/bitstyles/objects/button--icon/button--icon.stories.js
+++ scss/bitstyles/objects/button--icon/button--icon.stories.js
@@ -12,5 +12,13 @@ export default {
 export const buttonIcon = () => `
   <button class="o-button o-button--icon">
     <svg class="o-icon" viewBox="0 0 100 100" width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="m55.94 1.03v43h43v12h-43v43h-12v-43h-43v-12h43v-43z" fill-rule="evenodd"/></svg>
+    <span class="o-visuallyhidden">Button label</span>
+  </button>
+`;
+
+export const buttonIconDisabled = () => `
+  <button class="o-button o-button--icon" disabled>
+    <svg class="o-icon" viewBox="0 0 100 100" width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="m55.94 1.03v43h43v12h-43v43h-12v-43h-43v-12h43v-43z" fill-rule="evenodd"/></svg>
+    <span class="o-visuallyhidden">Button label</span>
   </button>
 `;
