7b6ffb956d65def8b3f1cc7b03b28b19fbab4bdf
Darren Cadwallader <darren@bitcrowd.net>
2021-02-24 17:55:51 +0100

    Changes the ordering of sections in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,4 +2,9 @@ import '../build/bitstyles.css';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  options: {
+    storySort: {
+      order: ['Introduction', ['Navigation', 'Content', 'Buttons', 'Data'], 'UI', 'Generic', 'Base', 'Layout', 'Objects', 'Components', 'Utilities'],
+    },
+  },
 }
