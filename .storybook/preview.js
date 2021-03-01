01a2b5717acfc0acadec6f73c3f15fcaf6e0a625
Darren Cadwallader <darren@bitcrowd.net>
2021-03-01 11:42:31 +0100

    Docs ordering in sidebar

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,7 +4,7 @@ export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   options: {
     storySort: {
-      order: ['Introduction', 'UI', ['Navigation', 'Content', 'Buttons', 'Data'], 'Utilities', 'Atoms', 'Organisms', 'Base' ],
+      order: ['Introduction', 'UI', ['Navigation', 'Content', 'Buttons', 'Data'], 'Atoms', 'Utilities', 'Organisms', 'Base' ],
     },
   },
 }
