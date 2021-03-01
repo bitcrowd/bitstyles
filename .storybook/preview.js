202ff8ade83afe21e93c7e9e3d6f564597c12dc4
Darren Cadwallader <darren@bitcrowd.net>
2021-03-01 12:31:02 +0100

    - Adds gh-pages package, and a couple of new commands for deploying to gh-pages to packagae.json - Storybook will now default to show docs (when they exist for a component), and docs come first - Updates storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,8 @@ import '../build/bitstyles.css';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  viewMode: 'docs',
+  previewTabs: { 'storybook/docs/panel': { index: -1 } },
   options: {
     storySort: {
       order: ['Introduction', 'UI', ['Navigation', 'Content', 'Buttons', 'Data'], 'Atoms', 'Utilities', 'Organisms', 'Base' ],
