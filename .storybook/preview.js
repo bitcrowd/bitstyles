632fb7696ecdef2a8e74fc01635ade6346bb61fd
Darren Cadwallader <darren@bitcrowd.net>
2021-03-02 09:19:47 +0100

    [#387] Structure the docs (#388)
    - Adds an intro page to storybook, listing all the components that are likely to be of use in admin sections
    - Restructures every high-level component into a folder structure in sotrybook to match the introduction page
    - renames components -> UI
    - Moves layout layer to utilities or objects
    - Renames objects -> atoms
    - Renames components -> organisms
    - Removes a couple of unused classes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,4 +2,9 @@ import '../build/bitstyles.css';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  options: {
+    storySort: {
+      order: ['Introduction', 'UI', ['Navigation', 'Content', 'Buttons', 'Data'], 'Atoms', 'Utilities', 'Organisms', 'Base' ],
+    },
+  },
 }
