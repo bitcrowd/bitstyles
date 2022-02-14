09518c523b6d85d0a4ab7f2fb1ab04be7e944954
Darren Cadwallader <darren@bitcrowd.net>
2022-02-14 09:03:43 +0100

    Reordering & organising docs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -10,6 +10,7 @@ export const parameters = {
     storySort: {
       order: [
         'Introduction',
+        ['Getting started', 'Principles', 'Extending bitstyles', 'UI components'],
         'Design Tokens',
         'UI',
         ['Navigation', 'Content', 'Buttons', 'Data'],
