79e44dd2536155e17f301a7c2ad9388e8afc289d
Darren Cadwallader <darren@bitcrowd.net>
2022-06-01 17:26:50 +0200

    - Reworks and simplifies the way the color palette storybook stories are generated - Also more generic, can now render base colors and semantic colors
     working on layout
    
    TODO:
    
    grid CSS shoul probably be a component, not utiltity

--- scss/bitstyles/ui/Colors.stories.js
+++ scss/bitstyles/ui/Colors.stories.js
@@ -1,4 +1,5 @@
-import palette from '../exports/_base-colors.scss';
+import baseColors from '../exports/_base-colors.scss';
+import colorPalette from '../exports/_color-palette.scss';
 
 /*
  * Variables in our Sass are exported via webpack in the form
@@ -17,60 +18,70 @@ import palette from '../exports/_base-colors.scss';
  *   …
  * ]
  */
-const objectToChunkedArray = (object, chunkSize) => {
-  const values = Object.values(object);
-  const array = [];
-  let counter = 0;
-  let portion = {};
 
-  // eslint-disable-next-line array-callback-return
-  Object.keys(object).map((key) => {
-    if (counter !== 0 && counter % chunkSize === 0) {
-      array.push(portion);
-      portion = {};
+function parseDotNotation(str, val, obj) {
+  let currentObj = obj;
+  const keys = str.split('.');
+  let i;
+  const l = Math.max(1, keys.length - 1);
+  let key;
+
+  for (i = 0; i < l; i += 1) {
+    key = keys[i];
+    currentObj[key] = currentObj[key] || {};
+    currentObj = currentObj[key];
+  }
+
+  currentObj[keys[i]] = val;
+  /* eslint-disable no-param-reassign */
+  delete obj[str];
+  /* eslint-enable no-param-reassign */
+}
+
+function expand(obj) {
+  let key;
+  /* eslint-disable no-restricted-syntax */
+  for (key in obj) {
+    if (key.indexOf('.') !== -1) {
+      parseDotNotation(key, obj[key], obj);
     }
-    portion[key] = values[counter];
-    counter += 1;
-  });
-  array.push(portion);
-  return array;
-};
+  }
+  /* eslint-enable no-restricted-syntax */
+  return obj;
+}
 
-const ColorItem = (label, color) =>
+const RenderColorItem = (label, color) =>
   `
-    <li style="background-color: ${color};">
-      <div style="min-height:4rem;"></div>
-      <span
-        class="u-bg-white u-padding-xxs-x u-margin-xxs-left"
+    <li style="background-color: ${color}; min-height:4rem;" class="u-aspect-ratio-1-1">
+      <div
+        class="u-margin-xs-left u-margin-xs-top"
         style="font-size: 10px;"
       >
-        ${label}
-      </span>
+        <strong class="u-bg-white u-padding-xxs-x">${label}</strong>
+        <br/>
+        <span class="u-bg-white u-padding-xxs-x">${color}</span>
+      </div>
     </li>
   `;
 
-const ColorRow = ({ colors }) =>
+const RenderColorRow = ({ colors }) =>
   `
-    <ul class="u-flex-grow-1 u-grid u-grid-cols-4 a-list-reset">
-      ${Object.keys(colors)
-        .map((color) => {
-          const colorNameParts = color.split('.');
-          const colorName = colorNameParts[colorNameParts.length - 1];
-          return ColorItem(colorName, colors[color]);
-        })
+    <ul class="u-flex-grow-1 u-grid u-grid-cols-auto a-list-reset">
+      ${Object.entries(colors)
+        .map((color) => RenderColorItem(color[0], color[1]))
         .join('')}
     </ul>
   `;
 
-const ColorPalette = () =>
-  `
+const RenderColorPalette = ({ palette }) => {
+  return `
     <ul class="a-list-reset u-grid u-gap-l u-grid-cols-2@m u-grid-cols-3@l">
-      ${objectToChunkedArray(palette, 25)
+      ${Object.entries(expand(palette))
         .map((colors) => {
-          const name = Object.keys(colors)[0].split('.')[0];
+          const name = colors[0];
           return `
           <li class="u-margin-xl-bottom u-grid u-gap-m">
-            ${ColorRow({ colors })}
+            ${RenderColorRow({ colors: colors[1] })}
             <h3
               class="u-margin-0-bottom u-margin-m-right u-line-height-min"
             >
@@ -82,10 +93,20 @@ const ColorPalette = () =>
         .join('')}
     </ul>
   `;
+};
 
-export { ColorPalette };
+const Template = (args) => RenderColorPalette(args);
+
+export const BaseColors = Template.bind({});
+BaseColors.args = {
+  palette: baseColors,
+};
+
+export const ColorPalette = Template.bind({});
+ColorPalette.args = {
+  palette: colorPalette,
+};
 
 export default {
   title: 'Base/Colors',
-  component: ColorPalette,
 };
