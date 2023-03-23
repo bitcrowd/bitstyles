e31f4821f561da577694f5fd24c71a55a527038b
Darren Cadwallader <darren@bitcrowd.net>
2023-03-23 10:07:14 +0100

    - Button--tab stories are now all in a js file, imported into their mdx overview - the button component now handles html content

--- scss/bitstyles/atoms/button/Button.js
+++ scss/bitstyles/atoms/button/Button.js
@@ -5,12 +5,13 @@ export default ({
   ariaPressed = false,
   ariaDisabled = false,
   ariaCurrent = false,
+  ariaSelected = false,
   disabled = false,
   element = 'button',
 }) => {
   const button = document.createElement(element);
   button.type = 'button';
-  button.innerText = children || 'Button';
+  button.innerHTML = children || 'Button';
   button.classList.add('a-button');
   colorVariant.forEach((variant) => {
     button.classList.add(`a-button--${variant}`);
@@ -21,6 +22,7 @@ export default ({
   if (ariaPressed) button.setAttribute('aria-pressed', ariaPressed);
   if (ariaDisabled) button.setAttribute('aria-disabled', ariaDisabled);
   if (ariaCurrent) button.setAttribute('aria-current', ariaCurrent);
+  if (ariaSelected) button.setAttribute('aria-selected', ariaSelected);
   if (disabled) button.setAttribute('disabled', disabled);
   return button;
 };
