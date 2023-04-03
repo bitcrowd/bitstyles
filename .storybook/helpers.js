460d81b7e39086962e910a635feaba538cd6868e
Darren Cadwallader <darren@bitcrowd.net>
2023-04-03 16:54:20 +0200

    - Filters the non-html-elements out of the base/typography output - Updates fixtures

--- .storybook/helpers.js
+++ .storybook/helpers.js
@@ -1,6 +1,7 @@
 export const remToPx = (rem, base = 16) => {
   let tempRem = rem;
-  if (typeof rem === 'string' || rem instanceof String) tempRem = tempRem.replace('rem', '');
+  if (typeof rem === 'string' || rem instanceof String)
+    tempRem = tempRem.replace('rem', '');
 
   tempRem = Number(tempRem);
   return base * tempRem;
