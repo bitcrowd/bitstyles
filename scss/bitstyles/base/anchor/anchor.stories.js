a605c5dec8ce3aa9a57c4f416c865d83705171a7
Darren Cadwallader <darren@bitcrowd.net>
2023-04-27 12:11:37 +0200

    Updates docs

--- scss/bitstyles/base/anchor/anchor.stories.js
+++ scss/bitstyles/base/anchor/anchor.stories.js
@@ -15,15 +15,30 @@ Base.parameters = {
   zeplinLink: [
     {
       name: 'base',
-      link: 'https://app.zeplin.io/styleguide/63079b90d0bf4a646c46c227/components?coid=63318432639e6b1c16b8acfe',
+      link: 'https://app.zeplin.io/styleguide/63079b90d0bf4a646c46c227/components?coid=643d4ab88305482390d7e721',
     },
     {
       name: 'hover',
-      link: 'https://app.zeplin.io/styleguide/63079b90d0bf4a646c46c227/components?coid=63318433c4678715d864a706',
+      link: 'https://app.zeplin.io/styleguide/63079b90d0bf4a646c46c227/components?coid=643d4abb122e2e235ff3ac3d',
     },
     {
       name: 'active',
-      link: 'https://app.zeplin.io/styleguide/63079b90d0bf4a646c46c227/components?coid=633184318a0b2514c945bbd4',
+      link: 'https://app.zeplin.io/styleguide/63079b90d0bf4a646c46c227/components?coid=643d4ab71ceb57241dc28ddb',
+    },
+    {
+      name: 'visited',
+      link: 'https://app.zeplin.io/styleguide/63079b90d0bf4a646c46c227/components?coid=643d4ca051e17223d3dd93d0',
+    },
+    {
+      name: 'focus-visible',
+      link: 'https://app.zeplin.io/styleguide/63079b90d0bf4a646c46c227/components?coid=643d4ab6d63f6e23f010ba93',
     },
   ],
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = { children: 'link text disabled', ariaDisabled: true };
+Disabled.parameters = {
+  zeplinLink:
+    'https://app.zeplin.io/styleguide/63079b90d0bf4a646c46c227/components?coid=643d4aba468e962435f34ab9',
 };
