8331d29a0fe293fc017bf619af26c4edbda29bb6
jeffrey <jeffrey@bitcrowd.net>
2023-07-25 01:31:24 +0100

    Update stories.

---
+++ scss/bitstyles/ui/SectionHeading.js
@@ -0,0 +1,52 @@
+export const Label = () => {
+  const span = document.createElement('span');
+  span.classList.add(
+    'u-fg-grayscale-light-1',
+    'u-font-normal',
+    'u-h6',
+    'u-margin-s1-left'
+  );
+  span.textContent = '[3/10]';
+  return span;
+};
+
+export default ({ title, label, extra = [] }) => {
+  const wrapper = document.createElement('div');
+  const extraContentContainer = document.createElement('div');
+  const heading = document.createElement('h3');
+
+  wrapper.classList.add(
+    'u-padding-m-bottom',
+    'u-border-grayscale-light-1-bottom',
+    'u-flex',
+    'u-items-center',
+    'u-justify-between',
+    'u-flex-wrap'
+  );
+
+  extraContentContainer.classList.add(
+    'u-flex',
+    'u-items-center',
+    'u-justify-between'
+  );
+
+  heading.classList.add(
+    'u-margin-0',
+    'u-margin-m-right',
+    'u-flex',
+    'u-items-center'
+  );
+  heading.textContent = title;
+  if (label) {
+    heading.appendChild(label);
+  }
+
+  wrapper.appendChild(heading);
+
+  extra.forEach((child) => {
+    extraContentContainer.append(child);
+  });
+
+  wrapper.appendChild(extraContentContainer);
+  return wrapper;
+};
