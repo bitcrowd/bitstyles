58ce04bb78a7c93e2fd95e76ae6b41cb08d5c4a5
Sabine Holler <sabine@bitcrowd.net>
2023-07-26 11:52:36 +0200

    Add improvements to sidebar

--- scss/bitstyles/organisms/sidebar/Sidebar.js
+++ scss/bitstyles/organisms/sidebar/Sidebar.js
@@ -33,12 +33,19 @@ listItem.classList.add('u-margin-s2-bottom', 'u-flex');
 
 const buttomComponent = (label) =>
   Button({
-    classname: ['u-justify-start', 'u-flex-grow-1'],
+    classname: ['u-justify-start'],
     children: `<svg width="20" height="20" class="a-icon a-icon--m" aria-hidden="true" focusable="false"><use xlink:href="${icons}#icon-caret-right"></use></svg><span class="u-margin-s1-left">${label}</span>`,
     colorVariant: ['transparent'],
   });
 
-const labels = ['Dashboards', 'Projects', 'Team', 'Search'];
+const labels = [
+  'Dashboard',
+  'Projects',
+  'Team',
+  'Customers',
+  'Bookings',
+  'Sales',
+];
 
 labels.forEach((label) =>
   buttonList.appendChild(listItem.appendChild(buttomComponent(label)))
@@ -69,33 +76,39 @@ bottom.innerHTML = `
     </div>
     <span class="a-button__label">Jane Dobermann</span>
   </button>
+    <ul
+    x-show="dropdownOpen"
+    @click.away="dropdownOpen = false"
+    id="dropdown-4"
+    class="a-dropdown a-dropdown--full-width a-dropdown--top u-overflow-y-auto u-list-none u-margin-s2-bottom"
+    >
+        <li>
+            <a href="/" class="a-button a-button--transparent"> Settings </a>
+        </li>
+        <li>
+            <a href="/" class="a-button a-button--transparent"> Help </a>
+        </li>
+        <li>
+            <a href="/" class="a-button a-button--transparent"> Privacy </a>
+        </li>
+        <li role="separator"></li>
+        <li>
+            <a href="/" class="a-button a-button--transparent"> Sign out </a>
+        </li>
+    </ul>
 </div>
 `;
 
-export default ({ topSlot, children, bottomSlot }) => {
-  // Create the main sidebar container
+const Sidebar = ({ topSlot, children, bottomSlot, mainContent = '' }) => {
   const sidebar = document.createElement('div');
-  sidebar.classList.add('u-flex', 'u-height-100vh');
-  sidebar.setAttribute('x-data', '{ sidebarOpen: false }');
+  sidebar.classList.add('u-flex', 'u-height-100vh', 'u-relative');
 
   const nav = document.createElement('nav');
   nav.classList.add('u-flex');
   sidebar.appendChild(nav);
 
-  const sidebarContentSmall = document.createElement('div');
-  sidebarContentSmall.classList.add(
-    'o-sidebar--small',
-    'u-bg-grayscale-light-4',
-    'u-shadow-brand-1',
-    'u-padding-m-top',
-    'u-flex',
-    'u-flex-col',
-    'u-hidden@l'
-  );
-  nav.appendChild(sidebarContentSmall);
-
-  const sidebarContentLarge = document.createElement('div');
-  sidebarContentLarge.classList.add(
+  const sidebarLarge = document.createElement('div');
+  sidebarLarge.classList.add(
     'u-hidden',
     'o-sidebar--large',
     'u-flex-shrink-0',
@@ -104,19 +117,100 @@ export default ({ topSlot, children, bottomSlot }) => {
     'u-flex@l',
     'u-flex-col'
   );
-  nav.appendChild(sidebarContentLarge);
+  nav.appendChild(sidebarLarge);
+
+  const sidebarSmall = document.createElement('div');
+  sidebarSmall.classList.add(
+    'o-sidebar--small',
+    'u-bg-grayscale-light-4',
+    'u-shadow-brand-1',
+    'u-flex',
+    'u-flex-col',
+    'u-hidden@l'
+  );
+  nav.appendChild(sidebarSmall);
+
+  let sidebarOpen = false;
+
+  const toggleSidebar = () => {
+    sidebarOpen = !sidebarOpen;
+    sidebarSmall.style.display = sidebarOpen ? 'flex' : 'none';
+  };
+
+  const closeButton = () =>
+    Button({
+      children: `<svg width="20" height="20" class="a-icon a-icon--m" aria-hidden="true" focusable="false"><use xlink:href="${icons}#icon-cross"></use></svg><span class="u-sr-only">Close Sidebar</span>`,
+      colorVariant: ['transparent'],
+      shapeVariant: ['square'],
+      onClick: () => toggleSidebar(),
+    });
+
+  sidebarSmall.appendChild(closeButton());
+
+  sidebarSmall.setAttribute('x-transition:enter', 'is-transitioning');
+  sidebarSmall.setAttribute('x-transition:enter-start', 'is-off-screen');
+  sidebarSmall.setAttribute('x-transition:enter-end', 'is-on-screen');
+  sidebarSmall.setAttribute('x-transition:leave', 'is-transitioning');
+  sidebarSmall.setAttribute('x-transition:leave-start', 'is-on-screen');
+  sidebarSmall.setAttribute('x-transition:leave-end', 'is-off-screen');
 
   if (topSlot) {
-    sidebarContentLarge.appendChild(topSlot.cloneNode(true));
-    sidebarContentSmall.appendChild(topSlot);
+    sidebarLarge.appendChild(topSlot.cloneNode(true));
+    sidebarSmall.appendChild(topSlot);
   }
   if (children) {
-    sidebarContentLarge.appendChild(children.cloneNode(true));
-    sidebarContentSmall.appendChild(children);
+    sidebarLarge.appendChild(children.cloneNode(true));
+    sidebarSmall.appendChild(children);
   }
   if (bottomSlot) {
-    sidebarContentLarge.appendChild(bottomSlot.cloneNode(true));
-    sidebarContentSmall.appendChild(bottomSlot);
+    sidebarLarge.appendChild(bottomSlot.cloneNode(true));
+    sidebarSmall.appendChild(bottomSlot);
   }
+
+  const main = document.createElement('main');
+  main.classList.add(
+    'u-flex-grow-1',
+    'u-padding-l4-left@l',
+    'u-padding-l3-right@l',
+    'u-padding-l3-bottom',
+    'u-padding-m',
+    'u-overflow-y-auto'
+  );
+  sidebar.appendChild(main);
+
+  const headerContainer = document.createElement('div');
+  headerContainer.classList.add(
+    'u-flex',
+    'u-items-center',
+    'u-margin-m-bottom'
+  );
+  main.appendChild(headerContainer);
+
+  const openButton = () =>
+    Button({
+      classname: [
+        'u-flex-shrink-0',
+        'u-margin-neg-m-left',
+        'u-margin-s4-right',
+        'u-hidden@l',
+      ],
+      children: `<svg width="20" height="20" class="a-icon a-icon--m" aria-hidden="true" focusable="false"><use xlink:href="${icons}#icon-hamburger"></use></svg><span class="u-sr-only">Open Sidebar</span>`,
+      colorVariant: ['secondary'],
+      shapeVariant: ['square'],
+      onClick: () => toggleSidebar(),
+    });
+  headerContainer.appendChild(openButton());
+
+  const header = document.createElement('h1');
+  header.classList.add('u-margin-0');
+  header.innerHTML = 'Projects';
+  headerContainer.appendChild(header);
+
+  const mainContentDiv = document.createElement('div');
+  mainContentDiv.innerHTML = mainContent;
+  main.appendChild(mainContentDiv);
+
   return sidebar;
 };
+
+export default Sidebar;
--- scss/bitstyles/organisms/sidebar/sidebar.stories.js
+++ scss/bitstyles/organisms/sidebar/sidebar.stories.js
@@ -6,16 +6,25 @@ export default {
   argTypes: {
     topSlot: {
       description:
-        'It is the top area of the sidebar, can be a compony logo for example',
+        'The top area of the sidebar, can be a compony logo for example',
     },
+    children: {
+      description:
+        'All the items that will go on the center of the sidebar, a list of buttons for example',
+    },
+    bottomSlot:
+      'The bottom area of the sidebar, a footer, or avatar link for example ',
   },
 };
 
+const mainContent = `Cotton candy chupa chups gummi bears cupcake candy canes sweet gummi bears macaroon lollipop. Danish toffee cheesecake chocolate bar jelly-o chocolate cake. Candy canes gummi bears pie fruitcake candy canes powder cheesecake. Jelly marshmallow marzipan apple pie jelly cupcake. Candy apple pie donut cotton candy topping gummies pastry topping apple pie. Gummies ice cream cookie pudding caramels candy canes pie. Ice cream macaroon halvah pastry lemon drops cheesecake.</p>`;
+
 const Template = (args) => Sidebar(args);
 
-export const DefaultPrimary = Template.bind({});
-DefaultPrimary.args = {
+export const Default = Template.bind({});
+Default.args = {
   topSlot: logoImg,
   children: buttonList,
   bottomSlot: bottom,
+  mainContent,
 };
