8baef3f51b6502d83c6e6010b198dbda30f20676
jeffrey <jeffrey@bitcrowd.net>
2024-03-18 14:16:56 +0100

    Add dropdown story variant.

--- scss/bitstyles/organisms/notification-center/NotificationCenter.stories.js
+++ scss/bitstyles/organisms/notification-center/NotificationCenter.stories.js
@@ -1,3 +1,4 @@
+import Dropdown from '../../atoms/dropdown/Dropdown';
 import NotificationCenter, { NotificationWrapper } from './NotificationCenter';
 
 export default {
@@ -20,4 +21,27 @@ Base.args = {
 Base.decorators = [baseDecorator];
 Base.parameters = {
   zeplinLink: 'https://zpl.io/WQKegrn',
+};
+
+export const InDropdown = Template.bind({});
+const dropdownDecorator = (story) => {
+  const decorator = document.createElement('div');
+  decorator.style.height = '30rem';
+  const children = `<ul class="u-list-none">
+  <li class="u-border-brand-1-dark-2-bottom">${story().outerHTML}</li>
+  <li class="u-border-brand-1-dark-2-bottom">${story().outerHTML}</li>
+  <li class="u-border-brand-1-dark-2-bottom">${story().outerHTML}</li>
+  <li>${story().outerHTML}</li>
+  </ul>`;
+  decorator.appendChild(Dropdown({ alignment: ['right'], children }));
+  return decorator;
+};
+
+InDropdown.args = {
+  title: 'Password update request confirmed',
+  subtitle: 'Thank you for updating your details.',
+};
+InDropdown.decorators = [dropdownDecorator];
+InDropdown.parameters = {
+  zeplinLink: 'https://zpl.io/Gn7L1d3',
 };
