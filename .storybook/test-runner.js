0445c5a686d39b881be0fe059d3ab0130f61aee6
Angelika Tyborska <angelika@bitcrowd.net>
2024-08-19 12:48:05 +0200

    Switch to axe-playwright

--- .storybook/test-runner.js
+++ .storybook/test-runner.js
@@ -1,12 +1,13 @@
-// TODO: try to make axe-playwright work or uninstall
-const { injectAxe, checkA11y, configureAxe } = require('axe-playwright');
 const { getStoryContext } = require('@storybook/test-runner');
-const { AxeBuilder } = require('@axe-core/playwright');
+const { injectAxe, checkA11y } = require('axe-playwright');
 /*
  * See https://storybook.js.org/docs/writing-tests/test-runner#test-hook-api
  * to learn more about the test-runner hooks API.
  */
 module.exports = {
+  async preVisit(page) {
+    await injectAxe(page);
+  },
   async postVisit(page, context) {
     // Get the entire context of a story, including parameters, args, argTypes, etc.
     const storyContext = await getStoryContext(page, context);
@@ -15,21 +16,15 @@ module.exports = {
     if (storyContext.parameters?.a11y?.disable) {
       return;
     }
-
-    const results = await new AxeBuilder({ page }).options({
-      // those rules fail for every story
-      rules: {
-        "region": { "enabled": false },
-        "landmark-one-main": { "enabled": false },
-        "page-has-heading-one": { "enabled": false }
-      }
-    }).analyze();
-
-    if (results.violations.length > 0) {
-      const storyId = `'${storyContext.title} ${storyContext.name}' (id: ${storyContext.id})`
-      console.error(`Story ${storyId} did not pass axe-core checks.\n\n`, JSON.stringify(results.violations, null, 2));
-
-      throw `Story ${storyId} did not pass axe-core checks. See test error logs for details.`
-    }
+    await checkA11y(
+      page,
+      '#root',
+      {
+        detailedReport: true,
+        verbose: false,
+      },
+      false,
+      'v2'
+    );
   },
 };
